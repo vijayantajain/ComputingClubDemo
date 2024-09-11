4205da85067add6d39605eb5b78452a706549468
Vijayanta Jain <vijayantajain@gmail.com>
2024-09-11 18:31:13 -0400

    Create a sum of lists

--- simple_math.py
+++ simple_math.py
@@ -3,8 +3,22 @@ print("hellow world!")
 print("Hello world: how are you?")
 
 
-def add(num_1: int, num_2: int):
-    return num_1 + num_2
+def add(num_1: int, num_2: int, num_3: float):
+    return (num_1 + num_2) * num_3
+
+
+some_list = [
+    1,
+    4,
+    4,
+    6,
+]
+
+sum = 0
+for i, each_num in enumerate(some_list):
+    sum += each_num
+
+print(sum)
 
 
 assert 7 == add(3, 4)
