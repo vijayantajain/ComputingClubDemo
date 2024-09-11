d05f91df5fcdb1c7e0c94d534b782b955756e98e
Vijayanta Jain <vijayantajain@gmail.com>
2024-09-11 18:25:59 -0400

    Write a function to add two numbers
    1. Removed extra print line
    2. Modified existing print line
    3. Created assert line

--- simple_math.py
+++ simple_math.py
@@ -1,5 +1,12 @@
 print("hellow world!")
 
-print("Hello world, how are you?")
+print("Hello world: how are you?")
 
-print("Hello world, how are you?")
+
+def add(num_1: int, num_2: int):
+    return num_1 + num_2
+
+
+assert 7 == add(3, 4)
+
+print("The sum of 3 and 4 is " + str(add(3, 4)))
